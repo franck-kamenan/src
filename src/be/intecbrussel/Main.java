327c743d3549e1f2709370304e18c91eba28e236
Franck Kamenan <franck.kamenan@yahoo.com>
2020-06-24 18:26:58 +0200

    via src

---
+++ src/be/intecbrussel/Main.java
@@ -0,0 +1,14 @@
+package be.intecbrussel;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 5;
+        int b = 6;
+        int c = 5;
+//String message = (a == c)? "it is true": "it is false";
+
+        System.out.println((a == b)? "it is true": "it is false");
+    }
+}
